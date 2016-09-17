b21e773bbc4457d21333d7c04018bcb63230b2c5
Neilsama <neilsamanyc@gmail.com>
2016-09-16 21:30:27 -0400

    week3 assignment

--- w02_h01_p5/effect.js
+++ w02_h01_p5/effect.js
@@ -1,9 +1,32 @@
-// $(document).ready(function(){
-//     // $(".button").click(animation());
-//     console.log("begin");
+$(document).ready(function(){
 
-// });
+    $("#button1").click(function(){
+    	$(this).toggleClass("selsect")
+    })
+    $("#button1").mouseenter(function(){
+    	$(this).animate({
+    		fontSize:'1.6em',
+    	},300)
+    })
+    $("#button1").mouseleave(function(){
+    	$(this).animate({
+    		fontSize:'1.5em',
+    	},300)
+    })
 
-// function animation(){
-// 	console.log("click");
-// }
+    $("#button2").click(function(){
+    	$(this).toggleClass("selsect")
+    })
+    $("#button2").mouseenter(function(){
+  	$(this).animate({
+    		fontSize:'1.6em',
+    	},300)
+    })
+    $("#button2").mouseleave(function(){
+    	$(this).animate({
+    		fontSize:'1.5em',
+    	},300)
+    })
+
+});
+
